5efe0ffe107c6a374e53876f8e886e2931cf1949
achevalier589 <alecchevalier6@gmail.com>
2013-03-04 13:23:50 -0600

    initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="styles.css" />
+<head>
+
+</head>
+<body>
+	<h2>Nebraska Football</h2>
+	<nav>
+		<ul>
+			<li><a>Team Info.</a></li>
+			<li><a>Roster</a></li>
+			<li><a>Add a Prospect/Recruit</a></li>
+			<li><a>Schedule</a></li>
+		</ul>
+	</nav>
+</body>
