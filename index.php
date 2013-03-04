c097e387c9b304b30f9fb266226f4c99191910f3
achevalier589 <alecchevalier6@gmail.com>
2013-03-04 13:38:56 -0600

    Added folders

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css" />
 <link rel="stylesheet" type="text/css" href="styles.css" />
 <head>
-
+	<link href="css/jimgMenu.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery-easing-1.3.pack.js"></script>
+	<script type="text/javascript" src="js/jquery-easing-compatibility.1.2.pack.js"></script>
 </head>
+
 <body>
-	<h2>Nebraska Football</h2>
-	<nav>
-		<ul>
-			<li><a>Team Info.</a></li>
-			<li><a>Roster</a></li>
-			<li><a>Add a Prospect/Recruit</a></li>
-			<li><a>Schedule</a></li>
-		</ul>
-	</nav>
+	<div class="jimgMenu">
+  		<ul>
+    		<li><a href="#">Landscapes</a></li>
+   			<li><a href="#">People</a></li>
+    		<li><a href="#">Nature</a></li>
+    		<li><a href="#">Abstract</a></li>
+    		<li><a href="#">Urban</a></li>
+  		</ul>
+	</div>
 </body>
+
---
+++ layout/nav.php
@@ -0,0 +1,9 @@
+<div class="jimgMenu">
+  <ul>
+    <li id="landscapes"><a href="#">Landscapes</a></li>
+    <li id="people"><a href="#">People</a></li>
+    <li id="nature"><a href="#">Nature</a></li>
+    <li id="abstract"><a href="#">Abstract</a></li>
+    <li id="urban"><a href="#">Urban</a></li>
+  </ul>
+</div>
