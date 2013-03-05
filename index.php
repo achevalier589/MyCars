faeb68e72a22a2ab5dc43d81f2e338ddfd07d8b2
unknown <rdethlefs850@wit-xdvm-086.MILLARDPS.NET>
2013-03-05 11:52:41 -0600

    including

--- index.php
+++ index.php
@@ -18,5 +18,20 @@
     			<li><a href="#">Add</a></li>
   			</ul>
 		</div>
+		
+		<div id="wrapper" class="container">
+			<header>
+				<?php include('layout/header.php')?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php')?>
+			</nav>
+			<section role="main">
+				<?php include('layout/main.php')?>
+			</section>
+			<footer>
+				<?php include('layout/footer.php')?> <!-- All content in the file listed -->
+			</footer>
+		</div>
 </body>
 
