456dd2268bf0f2d206f120aedf77086da4a16024
unknown <rdethlefs850@wit-xdvm-084.MILLARDPS.NET>
2013-03-06 13:02:26 -0600

    changes to pages

--- actions/add.php
+++ actions/add.php
@@ -1,11 +1,3 @@
-<?php session_start(); ?>
-<pre><?php print_r($_POST) ?></pre>
-<?php 
-require('../config/db.php');
-$required = array(
-	'car_name',	
-	'car_model',
-	'car_price'
-);
+
 
 
--- pages/favorites.php
+++ pages/favorites.php
@@ -1,1 +1,1 @@
-<?php
+<h1>My Favorite Cars</h1>
