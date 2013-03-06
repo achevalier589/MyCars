e04760822bdb36d96e66f63755372944eedfd56c
unknown <rdethlefs850@wit-xdvm-084.MILLARDPS.NET>
2013-03-06 11:55:49 -0600

    yeah

--- actions/add.php
+++ actions/add.php
@@ -1,1 +1,11 @@
-<?php
+<?php session_start(); ?>
+<pre><?php print_r($_POST) ?></pre>
+<?php 
+require('../config/db.php');
+$required = array(
+	'car_name',	
+	'car_model',
+	'car_price'
+);
+
+
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php
 
-//Import functions
-require('functions.php');
-?>
+
+
 <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css" />
 <link rel="stylesheet" type="text/css" href="styles.css" />
 <head>
