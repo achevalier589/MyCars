b0698cff5ae90d13bbf70bee981f3a511543ed10
unknown <rdethlefs850@wit-xdvm-084.MILLARDPS.NET>
2013-03-06 13:34:59 -0600

    changes to favorites page

--- pages/favorites.php
+++ pages/favorites.php
@@ -1,1 +1,18 @@
-<h1>My Favorite Cars</h1>
+<h1>Most Popular Cars</h1>
+<ol>
+	<li>Ferrari Enzo</li>	
+	<li>Lamborghini Aventador</li>
+	<li>Lamborghini Murcielago</li>
+	<li>Bugatti Veyron</li>
+	<li>Ferrari 458</li>
+	<li>Aston Martin</li>
+	<li>Jaguar XKR-S</li>
+	<li>Audi R8</li>
+	<li>Ford GT</li>
+	<li>Dodge Viper</li>
+	<li>Nissan GTR</li>	
+	<li>McLaren F1</li>
+	<li>Ford Mustang</li>
+	<li>Chevrolet Corvette</li>
+	<li>Maserati Granturismo</li>
+</ol>
