b336c8279454634696a0882ee1ac34fd7b059454
unknown <rdethlefs850@wit-xdvm-001.MILLARDPS.NET>
2013-03-07 13:34:12 -0600

    changed list on favorites page to a table and did some css editing

--- pages/favorites.php
+++ pages/favorites.php
@@ -3,52 +3,80 @@
 	<thead>
 		<tr>
 			<th>Car Type</th>
-			<th>Picture</th>
+			<th>Speed (0-60mph)</th>
 		</tr>
 		<tr>
-			<td>Ferrari Enzo</td>			
+			<td>Ferrari Enzo</td>
+			<td>2.4secs</td>
 		</tr>
 		<tr>
-			<td>Lamborghini Aventador</td>			
+			<td>Lamborghini Aventador</td>
+			<td>2.9secs</td>
 		</tr>
 		<tr>
-			<td>Lamborghini Murcielago</td>			
+			<td>Lamborghini Murcielago</td>
+			<td>3.0secs</td>
 		</tr>
 		<tr>
-			<td>Bugatti Veyron</td>			
+			<td>Bugatti Veyron</td>
+			<td>2.4secs</td>
 		</tr>
 		<tr>
-			<td>Ferrari 458</td>			
+			<td>Ferrari 458</td>
+			<td>3.1secs</td>
 		</tr>
 		<tr>
-			<td>Aston Martin</td>			
+			<td>Aston Martin</td>
+			<td>3.3secs</td>
 		</tr>
 		<tr>
-			<td>Jaguar XKR-S</td>			
+			<td>Jaguar XKR-S</td>
+			<td>3.6secs</td>
 		</tr>
 		<tr>
-			<td>Audi R8</td>			
+			<td>Audi R8</td>
+			<td>3.5secs</td>
 		</tr>
 		<tr>
-			<td>Ford GT</td>			
+			<td>Ford GT</td>
+			<td>3.4secs</td>
 		</tr>
 		<tr>
-			<td>Dodge Viper</td>			
+			<td>Dodge Viper</td>
+			<td>3.2secs</td>
 		</tr>
 		<tr>
-			<td>Nissan GTR</td>			
+			<td>Nissan GTR</td>
+			<td>3.5secs</td>
 		</tr>
 		<tr>
-			<td>McLaren F1</td>			
+			<td>McLaren F1</td>
+			<td>3.2secs</td>
 		</tr>
 		<tr>
-			<td>Ford Mustang</td>			
+			<td>Ford Mustang</td>
+			<td>3.9secs</td>
 		</tr>
 		<tr>
-			<td>Chevrolet Corvette</td>			
+			<td>Chevrolet Corvette</td>
+			<td>3.8secs</td>
 		</tr>
 		<tr>
-			<td>Maserati Granturismo</td>			
+			<td>Maserati Granturismo</td>
+			<td>3.1secs</td>
 		</tr>
+
+
+
+
 	</thead>
 </table>
+
+
+
+
+
+
+
+
+
